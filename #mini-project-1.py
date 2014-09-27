2153b6549d8a4d7302e5f6d2abec37a0a35ee22e
Ryan Bulloch <me@ryanbulloch.com>
2014-09-27 13:41:32 +0100

    Updated code, added comments and tested code, ready for submitting to Coursera.

--- #mini-project-1.py
+++ #mini-project-1.py
@@ -10,12 +10,16 @@
 # 3 - lizard
 # 4 - scissors
 
-# imported math function:
+# Author: Ryanish
 
+# --------------------------------------------
+
+# imported math function:
 from random import randrange
 
-# helper functions
+# helper functions:
 
+# convert name to number using if/elif/else
 def name_to_number(name):
     if name == "rock":
         return 0
@@ -30,6 +34,7 @@ def name_to_number(name):
     else:
         return "That is not an option - try again"
 
+ # convert number to a name using if/elif/else
 def number_to_name(number):
     if number == 0:
         return "rock"
@@ -44,30 +49,28 @@ def number_to_name(number):
     else:
         return "That is not an option - try again"
 
-
-
 def rpsls(player_choice):
-    print " "
+# print out the message for the player's choice
     print "Player chooses " + player_choice
+# convert the player's choice to player_number using the function name_to_number()
     name_to_number(player_choice)
 # compute random guess for comp_number using random.randrange()
     comp_number = randrange(0, 5)
-
 # convert comp_number to comp_choice using the function number_to_name()
     comp_choice = number_to_name(comp_number)
-
 # print out the message for computer's choice
     print "Computer chooses " + comp_choice
 # compute difference of comp_number and player_number modulo five
     difference = (name_to_number(player_choice) - comp_number) % 5
-# use if/elif/else to determine winner, print winner message
+# use if/elif/else to determine winner, print winner message - including the line break for the next set of messages
     if difference == 0:
-        print "Player and computer tie!"
+        print "Player and computer tie! \n"
     elif difference == 1 or difference == 2:
-        print "Player wins!"
+        print "Player wins! \n"
     else:
-        print "Computer wins!"
+        print "Computer wins! \n"
 
+# testing my code:
 rpsls("rock")
 rpsls("Spock")
 rpsls("paper")
