9c1764647c3f5b4e078679daa7f5e8a577f79550
Ryan Bulloch <me@ryanbulloch.com>
2014-10-02 16:05:01 +0100

    Update for project 2

--- #mini-project-2.py
+++ #mini-project-2.py
@@ -2,7 +2,12 @@
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
 
+import simplegui
+import random
+import math
 
+#initialised Globals
+number_range = 100
 
 # helper function to start and restart the game
 def new_game():
@@ -26,15 +31,24 @@ def range1000():
     
 def input_guess(guess):
     # main game logic goes here	
-    
+    global = operand
+    operand = int(guess) #?
+    print "Guess was" + guess
+    output()
     # remove this when you add your code
     pass
 
+
+#Add code to the event handler input_guess(guess) that takes the input string guess, converts it to an integer, and prints out a message of the form "Guess was 37" (or whatever the guess actually was). Hint: We have shown you how to convert strings to numbers in the lectures.
+
     
 # create frame
-
+frame = simplegui.create_frame("Guess the number game", 250, 175)
 
 # register event handlers for control elements and start frame
+frame.add_button("Range is (0, 100" + range100 + 200)
+frame.add_button("Range is (0, 1000" + range1000 + 200)
+frame.add_input("input field" + input_guess + 200)
 
 
 # call new_game 
