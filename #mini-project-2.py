1bf54856c2e96d5c5d49c58944d70a0442829546
Ryan Bulloch <ryan@rushcliff.com>
2014-10-02 17:46:02 +0100

    Completed task 2 or 5 minimum requirements

--- #mini-project-2.py
+++ #mini-project-2.py
@@ -16,7 +16,6 @@ def new_game():
     # remove this when you add your code    
     pass
 
-
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game 
@@ -28,19 +27,14 @@ def range1000():
     # button that changes the range to [0,1000) and starts a new game     
     
     pass
+
+#4. Add code to input_guess that compares the entered number to secret_number and prints out an appropriate message such as "Higher", "Lower", or "Correct".
     
 def input_guess(guess):
     # main game logic goes here 
-#    global = operand
-#    operand = int(guess) #?
-#    print "Guess was" + guess
-#    output()
-    # remove this when you add your code
-    pass
-
-
-#Add code to the event handler input_guess(guess) that takes the input string guess, converts it to an integer, and prints out a message of the form "Guess was 37" (or whatever the guess actually was). Hint: We have shown you how to convert strings to numbers in the lectures.
-
+     global number_range
+     number_range = int(guess)
+     print "Guess was", guess
     
 # create frame
 frame = simplegui.create_frame("Guess the number game", 250, 175)
@@ -51,8 +45,14 @@ frame.add_button("Range is (0, 1000", range1000, 200)
 frame.add_input("input field", input_guess, 200)
 
 
+#3. Add code to the function new_game that initializes a global variable secret_number to be a random number in the range [0, 100). Remember to include a global statement. Hint: look at the functions in the random module to figure out how to easily select such a random number. Note that the call to new_game() at the bottom of the template ensures that secret_number is always initialized when the program starts running.
+
+#When discussing ranges, we will follow the standard Python convention of including the low end of the range and excluding the high end of the range, which can be expressed mathematically as [low, high). So, [0, 3) means all of the numbers starting at 0 up to, but not including 3. In other words 0, 1, and 2. We suggest using the range [0, 100) in your first implementation.
+
 # call new_game 
 new_game()
 
 
 # always remember to check your completed program against the grading rubric
+
+#5. Test your code by playing multiple games of “Guess the number” with a fixed range. At this point, you will need to re-run your program between each game (using the CodeSkulptor “Run” button).  You are also welcome to use this testing template http://www.codeskulptor.org/#examples-gtn_testing_template.py. The bottom of the template contains a sequence of calls to input_guess() for three games of "Guess the number". Uncomment each sequence of calls and check whether the output in the console matches that provided in the comments below. Note that your output doesn't have to be identical, just of a similar form.
