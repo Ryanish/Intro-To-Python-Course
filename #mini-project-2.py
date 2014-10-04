17e5f047d13b2687c102da0bfb5fb35f84886bd2
Ryan Bulloch <me@ryanbulloch.com>
2014-10-04 13:32:45 +0100

    Update to work, hasn't got the guesses and limit in place yet, which is the final score on the rubric.

--- #mini-project-2.py
+++ #mini-project-2.py
@@ -4,50 +4,55 @@
 
 import simplegui
 import random
-import math
+# import math - Will I need this?
 
-#initialised Globals
-number_range = (0, 101)
+#initialised global variables
+number_range = 0
+secret_number = 0
 
-#3. Add code to the function new_game that initializes a global variable secret_number to be a random number in the range [0, 100). Remember to include a global statement. Hint: look at the functions in the random module to figure out how to easily select such a random number. Note that the call to new_game() at the bottom of the template ensures that secret_number is always initialized when the program starts running.
-
-# helper function to start and restart the game
+# function to start and restart the game
 def new_game():
-    # initialize global variables used in your code here
-    secret_number = random.randrange(0,101)
+    global number_range, secret_number
 
-
-# define event handlers for control panel
+# event handlers for control panel
 def range100():
-    # button that changes the range to [0,100) and starts a new game 
-    
-    # remove this when you add your code    
-    pass
+    # button that changes the range to [0,100) and starts a new game
+    # setting the global variables
+    global number_range, secret_number
+    number_range = (0, 101)
+    secret_number = random.randrange(0, 101)
+    # print the initial game text
+    print "New game. Range is from 0 to 100."
 
 def range1000():
-    # button that changes the range to [0,1000) and starts a new game     
-    
-    pass
-
-#4. Add code to input_guess that compares the entered number to secret_number and prints out an appropriate message such as "Higher", "Lower", or "Correct".
+    # button that changes the range to [0,1000) and starts a new game
+    # setting the global variables
+    global number_range, secret_number
+    number_range = (0, 1001)
+    secret_number = random.randrange(0, 1001)
+    # print the initial game text
+    print "New game. Range is from 0 to 1000."
     
 def input_guess(guess):
-    # main game logic goes here 
-     global number_range
-     number_range = int(guess)
-     print "Guess was", guess
+    global number_range, secret_number
+    # we want the users input guess string to be compared with the secret number function
+    print("Your guess is " + str(guess) + ".")
+    if int(guess) == secret_number:
+        print "Congrats!" 
+    elif int(guess) < secret_number:
+            print "Higher!"
+    elif int(guess) > secret_number:
+            print "Lower!"
     
-# create frame
+# create a window/frame
 frame = simplegui.create_frame("Guess the number game", 250, 175)
 
-# register event handlers for control elements and start frame
+# control elements for the window/frame
 frame.add_button("Range is (0, 100", range100, 200)
 frame.add_button("Range is (0, 1000", range1000, 200)
 frame.add_input("input field", input_guess, 200)
 
 # call new_game 
 new_game()
-
-# always remember to check your completed program against the grading rubric
-
-#5. Test your code by playing multiple games of “Guess the number” with a fixed range. At this point, you will need to re-run your program between each game (using the CodeSkulptor “Run” button).  You are also welcome to use this testing template http://www.codeskulptor.org/#examples-gtn_testing_template.py. The bottom of the template contains a sequence of calls to input_guess() for three games of "Guess the number". Uncomment each sequence of calls and check whether the output in the console matches that provided in the comments below. Note that your output doesn't have to be identical, just of a similar form.
+#initiate the 100 range game first so you don't need to click a button first
+range100()
