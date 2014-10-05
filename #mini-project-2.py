dd526947d6a20204d1a781d713bc64904966da95
Ryan Bulloch <me@ryanbulloch.com>
2014-10-05 10:32:07 +0100

    Final revision of the second week project.

--- #mini-project-2.py
+++ #mini-project-2.py
@@ -2,57 +2,92 @@
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
 
-import simplegui
-import random
-# import math - Will I need this?
+# Author: Ryanish
 
-#initialised global variables
+# Begin:
+
+import simplegui, random
+
+#initialise global variables
 number_range = 0
 secret_number = 0
+attempts_allowed = 0
 
-# function to start and restart the game
+# defined function to start and restart the game
 def new_game():
-    global number_range, secret_number
+    pass
 
-# event handlers for control panel
+# event handler functions
 def range100():
-    # button that changes the range to [0,100) and starts a new game
-    # setting the global variables
-    global number_range, secret_number
+    # this function changes the range to [0,100) and starts a new game
+    # setting the global variables and their values
+    global number_range, secret_number, attempts_allowed
     number_range = (0, 101)
     secret_number = random.randrange(0, 101)
-    # print the initial game text
+    attempts_allowed = 7
+    # print the initial game text to the player
     print "New game. Range is from 0 to 100."
 
 def range1000():
-    # button that changes the range to [0,1000) and starts a new game
-    # setting the global variables
-    global number_range, secret_number
+    # this function changes the range to [0,1000) and starts a new game
+    # setting the global variables and their values
+    global number_range, secret_number, attempts_allowed
     number_range = (0, 1001)
     secret_number = random.randrange(0, 1001)
-    # print the initial game text
+    attempts_allowed = 10
+    # print the initial game text to the player
     print "New game. Range is from 0 to 1000."
+
+def game_over():   
+    # If the user has 0 attempts left, end the current game and start a new one of the same range
+    if attempts_allowed == 0:
+        print "GAME OVER - the hidden number was", str(secret_number)
+        if number_range == (0, 101):
+            new_game()
+            range100()
+        else:
+            new_game()
+            range1000()
+    else: pass
     
 def input_guess(guess):
-    global number_range, secret_number
+    # setting the global variables 
+    global number_range, secret_number, attempts_allowed
     # we want the users input guess string to be compared with the secret number function
     print("Your guess is " + str(guess) + ".")
+    # if correct, you win
     if int(guess) == secret_number:
-        print "Congrats!" 
+        print "Congrats!"
+        if number_range == (0, 101):
+            new_game()
+            range100()
+        else:
+            new_game()
+            range1000()
+    # if incorrect, go higher and lose an attempt
     elif int(guess) < secret_number:
             print "Higher!"
+            attempts_allowed -= 1
+            print "You have", attempts_allowed, " attempts left!"
+            game_over()
+    # if incorrect, go lower and lose an attempt
     elif int(guess) > secret_number:
             print "Lower!"
+            attempts_allowed -= 1
+            print "You have", attempts_allowed, " attempts left!"
+            game_over()
     
-# create a window/frame
+# create a window/frame for the game
 frame = simplegui.create_frame("Guess the number game", 250, 175)
 
 # control elements for the window/frame
-frame.add_button("Range is (0, 100", range100, 200)
-frame.add_button("Range is (0, 1000", range1000, 200)
+frame.add_button("Number range is 0, 100", range100, 200)
+frame.add_button("Number range is 0, 1000", range1000, 200)
 frame.add_input("input field", input_guess, 200)
 
 # call new_game 
 new_game()
 #initiate the 100 range game first so you don't need to click a button first
 range100()
+
+# End of program
