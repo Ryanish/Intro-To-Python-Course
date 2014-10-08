e9dc46f54fd00c5fa0bfa620ee6f36bf385e786a
Ryan Bulloch <me@ryanbulloch.com>
2014-10-08 22:51:01 +0100

    completed stage 1 of project, working on stage 2

--- mini-project-3.py
+++ mini-project-3.py
@@ -3,7 +3,7 @@
 import simplegui
 
 # define global variables
-
+counter = 0
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -11,36 +11,40 @@ def format(t):
     pass
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
+def start():
+    timer.start()
+    
+def stop():
+    timer.stop()
 
-
-#timer.is.running()
+def reset():
+    global counter
+    counter = 0
 
 # define event handler for timer with 0.1 sec interval
 def timer_handler():
-    pass
+    global counter
+    print counter
+    counter += 1
+    
 # define draw handler
-
+def draw_handler(canvas):
+    canvas.draw_text("insert here", (80, 50), 20, 'White')
     
 # create frame
 frame = simplegui.create_frame("Stopwatch", 250, 175)
 frame.set_canvas_background('Black')
+frame.set_draw_handler(draw_handler)
+frame.add_button("Start", start, 100)
+frame.add_button("Stop", stop, 100)
+frame.add_button("Reset", reset, 100)
 frame.start()
 
 # register event handlers
-timer = simplegui.create_timer(500, timer_handler)
+timer = simplegui.create_timer(1000, timer_handler)
 
 
 # start frame
 
-timer.start()
-timer.stop()
 
 # Please remember to review the grading rubric
-"""
-1 pt - The program successfully opens a frame with the stopwatch stopped.
-1 pt - The program has a working "Start" button that starts the timer.
-1 pt - The program has a working "Stop" button that stops the timer.
-1 pt - The program has a working "Reset" button that stops the timer (if running) and resets the timer to 0.
-"""
-
-
