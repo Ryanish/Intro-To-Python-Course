d7448599f369938bf699b3f0ebf7419208cbab97
Ryan Bulloch <me@ryanbulloch.com>
2014-10-09 22:40:59 +0100

    finished stage 5 of the project.

--- mini-project-3.py
+++ mini-project-3.py
@@ -3,52 +3,58 @@
 import simplegui
 
 # define global variables
-counter = 0
+t = 0
+
+success_stop = 0
+total_stop = 0
+last_stop = 0
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
 def format(t):
-    pass
+    A = t // 600
+    B = t % 600 // 100
+    C = t % 100 // 10 
+    D = t % 10
+    return str(A) + ":" + str(B) + str(C) + "." + str(D) 
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
+    global t
     timer.start()
     
 def stop():
+    global t
     timer.stop()
 
 def reset():
-    global counter
-    counter = 0
+    global t
+    timer.stop()
+    t = 0
 
-# define event handler for timer with 0.1 sec interval
+# define event handler for timer with 0.1 sec interval - DONE
 def timer_handler():
-    global counter
-    print counter
-    counter += 1
+    global t
+    t = t + 1
     
 # define draw handler
-def draw_handler(canvas):
-    canvas.draw_text("it needs to go here", (80, 50), 20, 'White')
+def draw_stop_watch(canvas):
+    canvas.draw_text(format(t), (80, 50), 20, 'Red')
     
 # create frame
 frame = simplegui.create_frame("Stopwatch", 250, 175)
 frame.set_canvas_background('Black')
-frame.set_draw_handler(draw_handler)
 frame.add_button("Start", start, 100)
 frame.add_button("Stop", stop, 100)
 frame.add_button("Reset", reset, 100)
 frame.start()
 
+#register draw handler
+frame.set_draw_handler(draw_stop_watch)
+
 # register event handlers
-timer = simplegui.create_timer(1000, timer_handler)
+timer = simplegui.create_timer(100, timer_handler)
 
 
 # start frame
-"""
-Write the event handler function for the canvas that 
-draws the current time (simply as an integer, you
-should not worry about formatting it yet) in the middle
-of the canvas. Remember that you will need to convert the 
-current time into a string using str before drawing it.
-"""
+frame.start()
