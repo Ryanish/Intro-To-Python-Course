9b00fc3a6d1628ba5cb758a7943214ad2098cb0e
Ryan Bulloch <me@ryanbulloch.com>
2014-10-12 10:10:42 +0100

    Finished!

--- mini-project-3.py
+++ mini-project-3.py
@@ -1,45 +1,58 @@
 # template for "Stopwatch: The Game"
 
+# Author: Ryanish
+
 import simplegui
 
 # define global variables
-t = 0
 
-success_stop = 0
-total_stop = 0
+# main timer
+main_timer = 0
+# records successful watch stop
+successful_stop = 0
+# records the total number of stop button presses
+total_stops = 0
+# makes sure we can continue to keep recording total and successful stops and not mix us up with the 'main_timer' global variable
 last_stop = 0
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
-def format(t):
-    A = t // 600
-    B = t % 600 // 100
-    C = t % 100 // 10 
-    D = t % 10
+def format(main_timer):
+    A = main_timer // 600
+    B = main_timer % 600 // 100
+    C = main_timer % 100 // 10 
+    D = main_timer % 10
     return str(A) + ":" + str(B) + str(C) + "." + str(D) 
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
-    global t
     timer.start()
     
 def stop():
-    global t
+    global main_timer, successful_stop, total_stops, last_stop
     timer.stop()
+    if last_stop != main_timer:
+        total_stops = total_stops + 1
+        if main_timer % 10 == 0:
+            successful_stop = successful_stop + 1
+        last_stop = main_timer
 
 def reset():
-    global t
+    global main_timer, total_stops, last_stop
     timer.stop()
-    t = 0
+    main_timer = 0
+    total_stops = 0
+    successful_stop = 0
 
-# define event handler for timer with 0.1 sec interval - DONE
+# define event handler for timer with 0.1 sec interval
 def timer_handler():
-    global t
-    t = t + 1
+    global main_timer
+    main_timer = main_timer + 1
     
-# define draw handler
+# define draw handler - this shows the objects in the canvas frame
 def draw_stop_watch(canvas):
-    canvas.draw_text(format(t), (80, 50), 20, 'Red')
+    canvas.draw_text(format(main_timer), (65, 50), 52, '#fff')
+    canvas.draw_text(str(successful_stop) + " / " + str(total_stops), (95, 120), 40, '#ff0000')
     
 # create frame
 frame = simplegui.create_frame("Stopwatch", 250, 175)
@@ -52,7 +65,7 @@ frame.start()
 #register draw handler
 frame.set_draw_handler(draw_stop_watch)
 
-# register event handlers
+# register event handlers - the '100' digit is very important for displaying the correct equation on the watch
 timer = simplegui.create_timer(100, timer_handler)
 
 
